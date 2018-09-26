9ea7aae17e9cbd369e978b380c1c58361ab837a1
阿松 <hsong42@hotmail.com>
2018-09-26 15:08:27 +0800

    git bash

--- lab_3/binPack.m
+++ lab_3/binPack.m
@@ -1,3 +1,28 @@
 % Non linear optimization Lab 3
 % Problem 2
+clc;
+clear;
+close all;
 
+%%
+W=12;
+H=200;
+bin=zeros(H,W);
+% 生成N个箱子
+N=10;
+w=zeros(1,N);
+h=zeros(1,N);
+for k = 1:N
+    w(k)=randi(W); % 设计最大宽度为bin的宽度
+    h(k)=randi(W);
+end
+boxes=[w;h];
+
+% 绘制坐标轴
+figure(1);
+axis([0 W 0 H]);
+grid on;
+for k = 1:N
+    box=boxes(k);
+    
+end
